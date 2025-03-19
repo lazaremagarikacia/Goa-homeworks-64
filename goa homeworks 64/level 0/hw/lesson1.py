f60b3c60e32faa5b87b1a96ac3b001df10fa79b6
lazaremagarikacia <tigishvililazare15@gmail.com>
2025-03-19 10:36:16 -0700

    Add files via upload

---
+++ goa homeworks 64/level 0/hw/lesson1.py
@@ -0,0 +1,62 @@
+from turtle import *
+
+
+#we want to draw a house
+speed(100)
+width(5)
+color('green')
+forward (200)
+left (90)      
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward(200)
+#end of square
+#drawing a door
+left(90)
+forward(70)
+color('red')
+begin_fill()
+left(90)
+forward(100)
+right(90)
+forward(55)
+right(90)
+forward(100)
+penup()
+end_fill()
+goto(200,200)
+pendown()
+color('blue')
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+left(120)
+forward(55)
+right(90)
+forward(55)       
+right(90)
+forward(55)
+penup()
+goto(200,200)
+pendown()
+forward(55)
+left(90)
+forward(55)
+left(90)
+forward(55)
+
+
+
+
+
+
+
+
+
+
+exitonclick()
---
+++ goa homeworks 64/level 01/hw/lesson1.py
@@ -0,0 +1,30 @@
+name = 'lazare' #es aris str (string) tipis cvladi
+#name aris cvladi
+#= airs cvladistvis mnishvnelobis mimniwebeli simbolo
+#lazare aris cvladistvis mnishvneloba
+
+
+surname = 'tigishvili'
+
+
+
+print(surname)
+print(name)
+#print funqcias gadaecema ekranze gamosatani obieqti
+
+
+age = 13 #es aris int (integer) mteli ricxvi
+
+height = 59.5  #es aris float tipis cvladi (atwiladi)
+#boolean (bool) tipis cvladi
+
+knows_programming = True #True or False
+is_ugly = False #snakecase ubralod weris trili (standartuli)
+isUgly = False # javaskriptuli camelcase
+
+print(name + ' ' + surname)
+
+print(name + ' ' + surname + ' ' + str(age) + ' ' + str(height) )
+
+
+
